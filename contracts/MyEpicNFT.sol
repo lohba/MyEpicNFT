bdd34f339784dd41d40f836b55246d8f0cd76de1
lohba <lukeohba@gmail.com>
2021-11-22 21:37:38 -0800

    NFT image

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -10,14 +10,74 @@ contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    string baseSvg =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    string[] firstWords = [
+        "Blue",
+        "Red",
+        "Bisque",
+        "Laender",
+        "Khaki",
+        "Ivory",
+        "Thistle",
+        "Silver",
+        "Maroon",
+        "Teal",
+        "White"
+    ];
+    string[] secondWords = [
+        "Lemur",
+        "Cougar",
+        "Sloth",
+        "Salamander",
+        "Fox",
+        "Wolverine",
+        "Hamster",
+        "Newt",
+        "Parakeet",
+        "Mule"
+    ];
+    string[] thirdWords = [
+        "Pistol",
+        "Bazooka",
+        "ShotGun",
+        "Grenade",
+        "Rocket",
+        "Knife",
+        "Torpedo",
+        "AK47",
+        "Rifle",
+        "Sniper"
+    ];
+
     constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("This is my NFT contract. Woah!");
+    }
+
+    function pickRandomFirstWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
     }
 
     function makeAnEpicNFT() public {
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/CZMD");
+        _setTokenURI(
+            newItemId,
+            "data:application/json;base64,ewogICAgIm5hbWUiOiAiU2FsbW9uVG9mdUtpdHR5IiwKICAgICJkZXNjcmlwdGlvbiI6ICJBbiBORlQgZnJvbSB0aGUgaGlnaGx5IGFjY2xhaW1lZCBzcXVhcmUgY29sbGVjdGlvbiIsCiAgICAiaW1hZ2UiOiAiZGF0YTppbWFnZS9zdmcreG1sO2Jhc2U2NCxQSE4yWnlCNGJXeHVjejBpYUhSMGNEb3ZMM2QzZHk1M015NXZjbWN2TWpBd01DOXpkbWNpSUhCeVpYTmxjblpsUVhOd1pXTjBVbUYwYVc4OUluaE5hVzVaVFdsdUlHMWxaWFFpSUhacFpYZENiM2c5SWpBZ01DQXpOVEFnTXpVd0lqNEtJQ0FnSUR4emRIbHNaVDR1WW1GelpTQjdJR1pwYkd3NklIZG9hWFJsT3lCbWIyNTBMV1poYldsc2VUb2djMlZ5YVdZN0lHWnZiblF0YzJsNlpUb2dNVFJ3ZURzZ2ZUd3ZjM1I1YkdVK0NpQWdJQ0E4Y21WamRDQjNhV1IwYUQwaU1UQXdKU0lnYUdWcFoyaDBQU0l4TURBbElpQm1hV3hzUFNKaWJHRmpheUlnTHo0S0lDQWdJRHgwWlhoMElIZzlJalV3SlNJZ2VUMGlOVEFsSWlCamJHRnpjejBpWW1GelpTSWdaRzl0YVc1aGJuUXRZbUZ6Wld4cGJtVTlJbTFwWkdSc1pTSWdkR1Y0ZEMxaGJtTm9iM0k5SW0xcFpHUnNaU0krVTJGc2JXOXVWRzltZFV0cGRIUjVQQzkwWlhoMFBnbzhMM04yWno0SyIKfQo="
+        );
         console.log(
             "An NFT w/ ID %s has been minted to %s",
             newItemId,
@@ -26,4 +86,3 @@ contract MyEpicNFT is ERC721URIStorage {
         _tokenIds.increment();
     }
 }
-`
